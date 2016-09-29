d5b7bc2bda8558d315ae9ed1c531093907a47a98
vagrant <vagrant@precise64.(none)>
2016-09-29 20:54:56 +0000

    Create comment line ruby gem logloupe

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in logloupe.gemspec
+gem 'rake'
+gem 'rspec'
---
+++ lib/logloupe.rb
@@ -0,0 +1,12 @@
+require_relative "logloupe/version"
+module Logloupe
+  class Logloupe
+	def initialize
+		lines = 3
+        end
+
+	def say_hello
+		puts "test"
+	end
+  end
+end
